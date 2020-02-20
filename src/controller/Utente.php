04fb11b72c2d89f3ea511a17a48b8ea9bd659f71
rontinim <rontinim@users.noreply.github.com>
2020-02-20 08:58:23 +0100

    [ 0.1 ] - Magazzino Alberghetti - pulizia file

--- src/controller/Utente.php
+++ src/controller/Utente.php
@@ -2,9 +2,10 @@
     class Utente
     {
         public static function get_utente_by_email($email, $pswd){
+
             $profilo = Utente_model::get_utente_by_email($email, $pswd);
             
-            return ($profilo)? $profilo : False;
+            return ($profilo) ? $profilo : false;
         }
 
         public function richiesta_prodotti()

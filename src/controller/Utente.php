f8d6328cdf5045c0b224547ce57dbbd7396c4197
matteo310 <matteo3102001@gmail.com>
2020-02-20 09:05:28 +0100

    [ 0.2 ] - Magazzino Alberghetti - Controllo aggiornato

--- src/controller/Utente.php
+++ src/controller/Utente.php
@@ -1,26 +1,71 @@
 <?
+    require_once('Utente_model.php');
     class Utente
     {
-        public static function get_utente_by_email($email, $pswd){
+        public static function get_utente_by_email($email, $pswd)
+        {
             $profilo = Utente_model::get_utente_by_email($email, $pswd);
             
             return ($profilo)? $profilo : False;
         }
 
-        public function richiesta_prodotti()
-        {
-            //
+        public static function b(){
+            
         }
 
-        public function (){
+        public function c(){
 
         }
 
-        public function (){
+        public function d(){
 
         }
 
     }
-    
 
+    class Prodotto
+    {
+        public static function richiesta_prodotto()
+        {   //tutti possono richiedere i prodotti
+
+        }
+
+        public static function elimina_prodotto()
+        {   //soli i privilegiati
+
+        }
+
+        public static function aggiungi_prodotto()
+        {   //solo i privilegiati
+    
+        }
+
+        public static function visualizza_prodotto($id,$nomeProdotto)
+        {
+            $lista_prodotti = Prodotto_model::visualizza_prodotto($nomeProdotto);
+
+            //$set //probabilmente non serve siccome uno vuole l'elenco completo 
+
+            return ($lista_prodotti)? $lista_prodotti : false;
+        }
+
+        public static function riconsegna_prodotto()
+        {   //tutti possono riconsegnare
+            
+        }
+
+        public static function contaprodotto(){
+
+        }
+    }
+
+    class Categoria
+    {
+        public static function visualizza_categoria()
+        {
+            $lista_categoria = Categoria_model::visualizza_categoria();
+            return ($lista_categoria)? $lista_categoria : False; //da completare su html con tabella
+        }
+    
+    //menu tendina con ricerca categorie 
 ?>

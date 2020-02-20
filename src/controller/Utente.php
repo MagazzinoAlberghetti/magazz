860317bdcd3c222c988532a39f3dce42c4a9f3a8
matteo310 <matteo3102001@gmail.com>
2020-02-20 08:58:23 +0100

    [ 0.2 ] - Magazzino Alberghetti - control evolution
    add class product

--- src/controller/Utente.php
+++ src/controller/Utente.php
@@ -1,15 +1,16 @@
 <?
     class Utente
     {
-        public static function get_utente_by_email($email, $pswd){
+        public static function get_utente_by_email($email, $pswd)
+        {
             $profilo = Utente_model::get_utente_by_email($email, $pswd);
             
             return ($profilo)? $profilo : False;
         }
 
-        public function richiesta_prodotti()
+        public static function 
         {
-            //
+            
         }
 
         public function (){
@@ -21,6 +22,46 @@
         }
 
     }
-    
 
+    class Prodotto
+    {
+        public static function richiesta_prodotto()
+        {   //tutti possono richiedere i prodotti
+
+        }
+
+        public static function elimina_prodotto()
+        {   //soli i privilegiati
+
+        }
+
+        public static function aggiungi_prodotto()
+        {   //solo i privilegiati
+    
+        }
+
+        public static function visualizza_prodotto()
+        {
+            $lista_prodotto = Prodotto_model::visualizza_prodotto()  
+
+            $set = 
+
+            return 
+        }
+
+        public static function riconsegna_prodotto()
+        {   //tutti possono riconsegnare
+            
+        }
+    }
+
+    class Categoria
+    {
+        public static function visualizza_categoria()
+        {
+            $lista_categoria = Categoria_model::visualizza_categoria()
+            return ($lista_categoria)? $lista_categoria : False; //da completare su html con tabella
+        }
+    
+    //menu tendina con ricerca categorie 
 ?>
--- src/model/Utente_model.php
+++ src/model/Utente_model.php
@@ -1,13 +1,55 @@
 <?
     require_once('./connessione_database.php');
 
-    class Utente_model()
+    class Utente_model
     {
-        public function get_utente_by_email($email, $pass)
+        public function get_utente_by_email($email, $pswd)
         {
-            $data = $mydb->query("SELECT * FROM `.$db.`.$tabUtenti WHERE $tabUtenti.`email_utente`='$email' AND $tabUtenti.`password_utente`=PASSWORD('$pswd')");
+            $data = $mydb->query("SELECT * FROM `$db`.$tabUtenti WHERE $tabUtenti.`email_utente`='$email' AND $tabUtenti.`password_utente`=PASSWORD('$pswd')");
             return $data;
         }
+
+    }
+
+    class Prodotto_model
+    {
+        //get permessi
+
+        public static function richiesta_prodotto()
+        {   //tutti possono richiedere i prodotti
+            $quantita = $mydb->
+        }
+
+        public static function elimina_prodotto()
+        {   //soli i privilegiati
+
+        }
+
+        public static function aggiungi_prodotto()
+        {   //solo i privilegiati
+    
+        }
+
+        public static function visualizza_prodotto()
+        {   //tutti possono visualizzare
+            $data = $mydb->query("SELECT `nome_prodotto` FROM `$db`.$tabProdotto" );
+            return $data;
+        }
+
+        public static function riconsegna_prodotto()
+        {   //tutti possono riconsegnare
+            
+        }
+    }
+
+    class Categoria_model
+    {
+        public static function visualizza_categoria()
+        {
+            $data = $mydb->query("SELECT `nome_categoria` FROM `$db`.$tabCategoria" );
+            return $data;
+        }
+
 
     }
 
